8ec96497d035382a29d53a20f9816bc4648bdf66
Will Clarke <willclarke@Lappy.local>
2017-02-26 22:44:21 -0800

    Initial Commit

---
+++ WCForms/WCForms.h
@@ -0,0 +1,19 @@
+//
+//  WCForms.h
+//  WCForms
+//
+//  Created by Will Clarke on 2/26/17.
+//  Copyright © 2017 Will Clarke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WCForms.
+FOUNDATION_EXPORT double WCFormsVersionNumber;
+
+//! Project version string for WCForms.
+FOUNDATION_EXPORT const unsigned char WCFormsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WCForms/PublicHeader.h>
+
+
