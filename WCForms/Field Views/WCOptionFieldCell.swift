2dee66b1a1a9244cdaf61d5aad21074b9e74dfbe
Will Clarke <willclarke@d-108-179-176-25.dhcp4.washington.edu>
2017-04-25 12:58:58 -0700

    Removed inadvertently committed code that was commented out

--- WCForms/Field Views/WCOptionFieldCell.swift
+++ WCForms/Field Views/WCOptionFieldCell.swift
@@ -15,19 +15,4 @@ internal class WCOptionFieldCell: WCGenericFieldWithFieldNameCell {
 
 internal class WCOptionFieldRightDetailCell: WCGenericFieldRightDetailCell {
 
-//    let stackViewMarginSpacing: CGFloat = 16.0
-//    let minimumLabelWidth: CGFloat = 90.0
-//
-//    @IBOutlet weak var fieldNameAndValueStackView: UIStackView!
-//
-//    internal var proportionalWidthConstraint: NSLayoutConstraint? = nil
-//
-//    internal var fieldNameMinimumWidthConstraint: NSLayoutConstraint? = nil
-//
-//    internal var fieldValueMinimumWidthConstraint: NSLayoutConstraint? = nil
-
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//    }
-
 }
